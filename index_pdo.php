e8b7512c2f9299d068a5ef51272c2b298d5a4b95
Lilia <geertszelia@gmail.com>
2017-01-31 16:23:52 +0100

    Ajout des paramètre de requete

--- index_pdo.php
+++ index_pdo.php
@@ -1,7 +1,7 @@
 <?php
 	$bdd = new PDO('mysql:host=localhost;dbname=mon_armoire;charset=utf8', 'root', 'simplonco');
-	$reponse = $bdd->query('SELECT * FROM mes_chaussettes WHERE couleur=\'bleu\' && pointure>40');
-?>\
+	$reponse = $bdd->query('SELECT * FROM mes_chaussettes WHERE couleur=\''.$_GET['couleur'].'\' && pointure=\''.$_GET['pointure'].'\' && temp_lavage=\''.$_GET['temp_lavage'].'\'');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
